d0735423f33afe11bd3020eea843c72986ba1b55
Xavi-PL <xavipl.mvk25@gmail.com>
2021-03-29 11:40:33 +0200

    first commit

---
+++ app/src/main/java/com/wakeapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wakeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.rvDevices);
+
+
+    }
+}
